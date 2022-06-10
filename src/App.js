ce3c4b7b978aa4587c0580b983860fc44eaf059a
Denis <molotov.ant@mail.ru>
2022-06-10 16:03:52 +0500

    r

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import TrackIitems from "./components/Track_items";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      "X-RapidAPI-Key": "ca03dc8b4amsh8fe919ef52202dbp1412f2jsn8b0561de19b7",
+    },
+  };
+  let [value, setValue] = useState();
+  let [data, setData] = useState([]);
+
+  function getApi() {
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?" + value, options)
+      .then((response) => response.json())
+      .then((response) => handleApi(response));
+  }
+
+  function handleApi(res) {
+    if (res.error || !res) getApi();
+    else {
+      //setData([...data, ...res.data]);
+      res.data.pop();
+      setData(res.data);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <div className="wrapper">
+        <h2>Введите запрос</h2>
+        <div>
+          <input
+            type="text"
+            placeholder="Oxxxymiron"
+            onChange={(event) => setValue("q=" + event.currentTarget.value)}
+          ></input>
+          <button type="submit" onClick={getApi}>
+            ОТПРАВИТЬ
+          </button>
+        </div>
+        <audio src="" controls autoPlay loop></audio>
+        <div className="items">
+          <TrackIitems data={data} />
+        </div>
+      </div>
     </div>
   );
 }
